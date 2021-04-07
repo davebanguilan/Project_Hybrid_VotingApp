8c4de3ab36c65c8c2c95479fda0fecb3b3f84acb
davebanguilan <desbanguilan@gmail.com>
2021-04-07 17:24:51 +0800

    :refactor: Refactor: Change baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "https://localhost:44351/",
+    baseURL: "https://votingappbackend.azurewebsites.net/",
 });
 
 API.interceptors.request.use((req) => {
